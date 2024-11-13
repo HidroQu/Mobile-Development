f26049d86af89b75624e1e2018a5af05219ff856
muammarRM <muammarramadhanimaulizidan@gmail.com>
2024-11-13 20:16:29 +0700

    feat: add navigation

--- app/src/main/java/com/capstone/hidroqu/ui/article/ArticleActivity.kt
+++ app/src/main/java/com/capstone/hidroqu/ui/article/ArticleActivity.kt
@@ -1,22 +1,33 @@
 package com.capstone.hidroqu.ui.article
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.capstone.hidroqu.ui.home.AlarmSection
+import com.capstone.hidroqu.ui.home.ArticleSection
+import com.capstone.hidroqu.ui.home.CameraSection
+import com.capstone.hidroqu.ui.home.TopHome
 import com.capstone.hidroqu.ui.theme.HidroQuTheme
 
 @Composable
 fun ArticleActivity(modifier: Modifier = Modifier) {
-
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+            .padding(20.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        ArticleSection()
+    }
 }
 
 @Preview(device = Devices.DEFAULT, showBackground = true)
