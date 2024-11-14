6b806b0c0fd70c334f58370625e1572f9ada0b2e
Chwakillah <syakillahnachwa081@gmail.com>
2024-11-14 18:21:42 +0700

    feat(register): form register

--- app/src/main/java/com/capstone/hidroqu/MainActivity.kt
+++ app/src/main/java/com/capstone/hidroqu/MainActivity.kt
@@ -6,21 +6,14 @@ import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
-import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -29,14 +22,12 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,9 +38,9 @@ import androidx.navigation.compose.rememberNavController
 import com.capstone.hidroqu.ui.comunity.ComunityActivity
 import com.capstone.hidroqu.ui.home.HomeActivity
 import com.capstone.hidroqu.ui.myplant.MyPlantActivity
-import com.capstone.hidroqu.ui.profile.ProfileActivity
 import com.capstone.hidroqu.ui.theme.HidroQuTheme
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.capstone.hidroqu.ui.register.RegisterAvtivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -96,10 +87,19 @@ fun MainApp() {
             composable("Home") { HomeActivity() }
             composable("Tanmanku") { MyPlantActivity() }
             composable("Komunitas") { ComunityActivity() }
-            composable("Profil") { ProfileActivity() }
+            composable("Profil") { RegisterAvtivity(
+                name = "", // Passing initial values
+                email = "",
+                password = "",
+                checkValid = mutableListOf(),
+                onNameChanged = {},
+                onEmailChanged = {},
+                onPasswordChanged = {},
+                onLoginClicked = {},
+                onRegisterClicked = {},
+                onBackClick = {} ) } }
         }
     }
-}
 
 @Composable
 fun BottomNavigationBar(navController: NavHostController) {
