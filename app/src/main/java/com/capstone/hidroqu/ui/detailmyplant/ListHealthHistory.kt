834caa16abd7767bb1111ed3404ccea734ad1c4d
muammarRM <muammarramadhanimaulizidan@gmail.com>
2024-11-16 00:21:21 +0700

    feat(health history): add health history screen

--- app/src/main/java/com/capstone/hidroqu/ui/detailmyplant/ListHealthHistory.kt
+++ app/src/main/java/com/capstone/hidroqu/ui/detailmyplant/ListHealthHistory.kt
@@ -4,29 +4,66 @@ import com.capstone.hidroqu.R
 
 
 data class ListHealthHistory(
-    val id: Int,
+    val plantId: Int,
+    val healthId: Int, //primary
     val issue: String,
-    val date: String,
+    val dateHistory: String,
     val symptoms: String,
     val cause: String,
-    val userPlantPhoto: Int,
-    val relatedPhoto: Int
+    val relatedPhotos: List<Int>,
+    val userPlantPhoto: Int
 )
+
+
 val dummyListHealthHistory = listOf(
-    ListHealthHistory(1,
-        "Kekurangan Sulfur",
-        "04 November 2024",
-        "Tanaman yang mengalami kekurangan sulfur menunjukkan gejala daun muda menguning (klorosis), mirip dengan gejala kekurangan nitrogen, tetapi dimulai dari daun-daun muda terlebih dahulu. Sulfur diperlukan untuk pembentukan protein dan enzim, sehingga kekurangan sulfur dapat menghambat pertumbuhan tanaman, menghasilkan daun yang lebih kecil, dan batang yang lebih lemah. Pada tanaman dengan kekurangan sulfur yang parah, seluruh tanaman dapat tampak kuning atau pucat, dan hasil panen pun cenderung menurun.",
-        "Penurunan Kadar Sulfur dalam Larutan Nutrisi Seiring Waktu\nTanaman akan menyerap sulfur dari larutan nutrisi, dan jika larutan tidak diganti secara berkala, konsentrasi sulfur akan menurun. Ini dapat menyebabkan kekurangan sulfur, terutama pada tahap pertumbuhan cepat.\n\nKetidaksesuaian pH dalam Larutan Nutrisi\npH yang terlalu tinggi atau rendah dapat menghambat penyerapan sulfur dan nutrisi lainnya oleh tanaman. Dalam hidroponik, pH optimal untuk tanaman timun adalah antara 5.5 dan 6.5. Di luar rentang ini, akar tanaman mungkin kesulitan menyerap sulfur meskipun unsur ini tersedia dalam larutan.",
-        R.drawable.poto_tanam,
-        R.drawable.scan_tanam
+    ListHealthHistory(
+        plantId = 1,
+        healthId = 1,
+        issue = "Kekurangan sulfur 1",
+        dateHistory = "04 November 2024",
+        symptoms = "Tanaman yang mengalami kekurangan sulfur menunjukkan gejala daun muda menguning (klorosis), mirip dengan gejala kekurangan nitrogen, tetapi dimulai dari daun-daun muda terlebih dahulu.",
+        cause = "Penurunan Kadar Sulfur dalam Larutan Nutrisi Seiring Waktu\nTanaman akan menyerap sulfur dari larutan nutrisi, dan jika larutan tidak diganti secara berkala, konsentrasi sulfur akan menurun. Ini dapat menyebabkan kekurangan sulfur, terutama pada tahap pertumbuhan cepat.",
+        relatedPhotos = listOf(R.drawable.ic_launcher_background, R.drawable.scan_tanam, R.drawable.poto_tanam, R.drawable.ic_launcher_background),
+        userPlantPhoto = R.drawable.scan_tanam
     ),
-    ListHealthHistory(2,
-        "Kekurangan Sulfur",
-        "04 November 2024",
-        "Tanaman yang mengalami kekurangan sulfur menunjukkan gejala daun muda menguning (klorosis), mirip dengan gejala kekurangan nitrogen, tetapi dimulai dari daun-daun muda terlebih dahulu. Sulfur diperlukan untuk pembentukan protein dan enzim, sehingga kekurangan sulfur dapat menghambat pertumbuhan tanaman, menghasilkan daun yang lebih kecil, dan batang yang lebih lemah. Pada tanaman dengan kekurangan sulfur yang parah, seluruh tanaman dapat tampak kuning atau pucat, dan hasil panen pun cenderung menurun.",
-        "Penurunan Kadar Sulfur dalam Larutan Nutrisi Seiring Waktu\nTanaman akan menyerap sulfur dari larutan nutrisi, dan jika larutan tidak diganti secara berkala, konsentrasi sulfur akan menurun. Ini dapat menyebabkan kekurangan sulfur, terutama pada tahap pertumbuhan cepat.\n\nKetidaksesuaian pH dalam Larutan Nutrisi\npH yang terlalu tinggi atau rendah dapat menghambat penyerapan sulfur dan nutrisi lainnya oleh tanaman. Dalam hidroponik, pH optimal untuk tanaman timun adalah antara 5.5 dan 6.5. Di luar rentang ini, akar tanaman mungkin kesulitan menyerap sulfur meskipun unsur ini tersedia dalam larutan.",
-        R.drawable.poto_tanam,
-        R.drawable.scan_tanam
+    ListHealthHistory(
+        plantId = 1,
+        healthId = 2,
+        issue = "Kekurangan sulfur 2",
+        dateHistory = "04 November 2024",
+        symptoms = "Tanaman yang mengalami kekurangan sulfur menunjukkan gejala daun muda menguning (klorosis), mirip dengan gejala kekurangan nitrogen, tetapi dimulai dari daun-daun muda terlebih dahulu.",
+        cause = "Penurunan Kadar Sulfur dalam Larutan Nutrisi Seiring Waktu\nTanaman akan menyerap sulfur dari larutan nutrisi, dan jika larutan tidak diganti secara berkala, konsentrasi sulfur akan menurun. Ini dapat menyebabkan kekurangan sulfur, terutama pada tahap pertumbuhan cepat.",
+        relatedPhotos = listOf(R.drawable.ic_launcher_background, R.drawable.scan_tanam, R.drawable.poto_tanam, R.drawable.ic_launcher_background),
+        userPlantPhoto = R.drawable.scan_tanam
     ),
+    ListHealthHistory(
+        plantId = 1,
+        healthId = 3,
+        issue = "Kekurangan sulfur 3",
+        dateHistory = "04 November 2024",
+        symptoms = "Tanaman yang mengalami kekurangan sulfur menunjukkan gejala daun muda menguning (klorosis), mirip dengan gejala kekurangan nitrogen, tetapi dimulai dari daun-daun muda terlebih dahulu.",
+        cause = "Penurunan Kadar Sulfur dalam Larutan Nutrisi Seiring Waktu\nTanaman akan menyerap sulfur dari larutan nutrisi, dan jika larutan tidak diganti secara berkala, konsentrasi sulfur akan menurun. Ini dapat menyebabkan kekurangan sulfur, terutama pada tahap pertumbuhan cepat.",
+        relatedPhotos = listOf(R.drawable.ic_launcher_background, R.drawable.scan_tanam, R.drawable.poto_tanam, R.drawable.ic_launcher_background),
+        userPlantPhoto = R.drawable.scan_tanam
+    ),
+    ListHealthHistory(
+        plantId = 2,
+        healthId = 1,
+        issue = "Kekurangan vitamin a",
+        dateHistory = "30 Oktober 2024",
+        symptoms = "Tanaman yang mengalami kekurangan sulfur menunjukkan gejala daun muda menguning (klorosis), mirip dengan gejala kekurangan nitrogen, tetapi dimulai dari daun-daun muda terlebih dahulu.",
+        cause = "Penurunan Kadar Sulfur dalam Larutan Nutrisi Seiring Waktu\nTanaman akan menyerap sulfur dari larutan nutrisi, dan jika larutan tidak diganti secara berkala, konsentrasi sulfur akan menurun. Ini dapat menyebabkan kekurangan sulfur, terutama pada tahap pertumbuhan cepat.",
+        relatedPhotos = listOf(R.drawable.ic_launcher_background, R.drawable.scan_tanam, R.drawable.poto_tanam, R.drawable.ic_launcher_background),
+        userPlantPhoto = R.drawable.scan_tanam
+    )
 )
+
+
+fun getHealthHistoryByPlantId(plantId: Int): List<ListHealthHistory> {
+    return dummyListHealthHistory.filter { it.plantId == plantId }
+}
+
+fun getHealthHistoryById(healthId: Int): ListHealthHistory? {
+    // Dummy data for example. Replace with your actual data fetching logic.
+    return dummyListHealthHistory.find { it.healthId == healthId }
+}
