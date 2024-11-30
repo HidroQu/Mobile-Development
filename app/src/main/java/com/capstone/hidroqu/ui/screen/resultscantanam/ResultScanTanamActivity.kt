da191609820446632cef582d147a53170c638cd2
muammarRM <muammarramadhanimaulizidan@gmail.com>
2024-11-30 21:17:04 +0700

    fetch: ScanTanam to ML API

--- app/src/main/java/com/capstone/hidroqu/ui/screen/resultscantanam/ResultScanTanamActivity.kt
+++ app/src/main/java/com/capstone/hidroqu/ui/screen/resultscantanam/ResultScanTanamActivity.kt
@@ -1,6 +1,9 @@
 package com.capstone.hidroqu.ui.screen.resultscantanam
 
+import android.content.Context
 import android.net.Uri
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -8,26 +11,68 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import com.capstone.hidroqu.navigation.Screen
 import com.capstone.hidroqu.navigation.SimpleLightTopAppBar
+import com.capstone.hidroqu.nonui.data.UserPreferences
 import com.capstone.hidroqu.ui.theme.HidroQuTheme
+import com.capstone.hidroqu.ui.viewmodel.ScanPlantViewModel
 
 @Composable
 fun ResultScanTanamActivity(
     photoUri: String?,
     navHostController: NavHostController,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: ScanPlantViewModel = viewModel(),
+    context: Context = LocalContext.current
 ) {
-    val imageUri = Uri.parse(photoUri)
+    val uri = Uri.parse(photoUri)
+    val userPreferences = UserPreferences(context)
+    val token by userPreferences.token.collectAsState(initial = null)
+    val isLoading by viewModel.isLoading.collectAsState(false)
+    val errorMessage by viewModel.errorMessage.collectAsState("")
+    val plantPrediction by viewModel.plantPrediction.collectAsState()
+
+    LaunchedEffect(uri) {
+        Log.d("ResultPotoTanamActivity", "URI: $uri")
+        token?.let {
+            Log.d("ResultPotoTanamActivity", "Token found: $it")
+            if (uri != null) {
+                Log.d("ResultPotoTanamActivity", "Starting nutrient prediction...")
+                viewModel.predictPlant(token!!, uri, context) { response ->
+                    Log.d("ResultPotoTanamActivity", "Nutrient prediction success: $response")
+                }
+            } else {
+                Log.e("ResultPotoTanamActivity", "URI is null, navigating to home...")
+                navHostController.navigate(Screen.Home.route)
+            }
+        } ?: run {
+            Log.e("ResultPotoTanamActivity", "Token is null, navigating to home...")
+            navHostController.navigate(Screen.Home.route)
+        }
+    }
+
+    // Handle error cases
+    LaunchedEffect(errorMessage) {
+        if (errorMessage?.isNotEmpty() == true) {
+            Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show()
+            navHostController.navigate(Screen.Home.route)  // Navigate to Home if error occurs
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -57,6 +102,16 @@ fun ResultScanTanamActivity(
             }
         }
     ) { paddingValues ->
+        if (isLoading) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        } else {
         Column(
             modifier = Modifier
                 .verticalScroll(rememberScrollState())
@@ -73,7 +128,7 @@ fun ResultScanTanamActivity(
                 horizontalArrangement = Arrangement.spacedBy(24.dp)
             ) {
                 AsyncImage(
-                    model = imageUri,
+                    model = photoUri,
                     contentDescription = "User Photo",
                     modifier = Modifier
                         .height(150.dp)
@@ -94,7 +149,7 @@ fun ResultScanTanamActivity(
                             color = MaterialTheme.colorScheme.onPrimaryContainer
                         )
                         Text(
-                            text = "Timun",
+                            text = plantPrediction?.data?.plant?.name ?: "",
                             style = MaterialTheme.typography.bodyMedium,
                             color = MaterialTheme.colorScheme.onPrimaryContainer
                         )
@@ -108,7 +163,7 @@ fun ResultScanTanamActivity(
                             color = MaterialTheme.colorScheme.onPrimaryContainer
                         )
                         Text(
-                            text = "Cucumis sativus",
+                            text = plantPrediction?.data?.plant?.latin_name ?: "",
                             style = MaterialTheme.typography.bodyMedium,
                             color = MaterialTheme.colorScheme.onPrimaryContainer
                         )
@@ -133,7 +188,7 @@ fun ResultScanTanamActivity(
                         color = MaterialTheme.colorScheme.onPrimaryContainer
                     )
                     Text(
-                        text = "Mentimun, timun, atau ketimun (Cucumis sativus) merupakan tumbuhan yang menghasilkan buah yang dapat dimakan.",
+                        text = plantPrediction?.data?.plant?.description ?: "",
                         style = MaterialTheme.typography.bodyMedium,
                         color = MaterialTheme.colorScheme.onPrimaryContainer
                     )
@@ -144,15 +199,12 @@ fun ResultScanTanamActivity(
                     verticalArrangement = Arrangement.spacedBy(4.dp)
                 ) {
                     Text(
-                        text = "Tanaman Sejenis",
+                        text = "Nutrisi Hidroponik",
                         style = MaterialTheme.typography.labelLarge,
                         color = MaterialTheme.colorScheme.onPrimaryContainer
                     )
                     Text(
-                        text = "1. Timun Armenia\n" +
-                                "2. Mentimun Libanon\n" +
-                                "3. Timun Inggris\n" +
-                                "4. Timun Persia",
+                        text = plantPrediction?.data?.plant?.fertilizer_type ?: "",
                         style = MaterialTheme.typography.bodyMedium,
                         color = MaterialTheme.colorScheme.onPrimaryContainer
                     )
@@ -173,22 +225,45 @@ fun ResultScanTanamActivity(
                     style = MaterialTheme.typography.labelLarge,
                     color = MaterialTheme.colorScheme.onPrimaryContainer)
                 Text(
-                    text = "1. Buat gundukan tanah berukuran enam sampai delapan inci dan taruh tiga biji mentimun dalam bentuk segitiga.\n" +
-                            "2. Beri jarak yang sesuai karena tanaman bisa tumbuh cukup besar.\n" +
-                            "3. Mentimun menyukai suhu hangat dan sinar matahari langsung.\n" +
-                            "4. Siapkan wadah persemaian dan isi dengan pasir.\n" +
-                            "5. Buat alur tanam dengan kedalaman 1 cm dan jarak alur sekitar 5 cm.\n" +
-                            "6. Sebar benih timun pada alur tanam dan tutup kembali dengan pasir.\n" +
-                            "7. Siram sampai media semai lembap.\n" +
-                            "8. Pasang lanjaran 2-3 hari sebelum tanam, paling lambat sampai sebelum sulur keluar.\n" +
-                            "9. Buat lanjaran setinggi 20-30 cm kemudian diikat.\n" +
-                            "10. Transplanting dilakukan pada sore hari sekitar jam 2-3 sore.",
+                    text = plantPrediction?.data?.plant?.planting_guide ?: "",
                     style = MaterialTheme.typography.bodyMedium,
                     color = MaterialTheme.colorScheme.onPrimaryContainer
                 )
             }
+            // Cara Menanam
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(MaterialTheme.colorScheme.onPrimary)
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                Text(
+                    text = "Fun Fact",
+                    style = MaterialTheme.typography.labelLarge,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer)
+                Text(
+                    text = buildAnnotatedString {
+                        append(plantPrediction?.data?.plant?.fun_fact ?: "Fakta tanaman belum tersedia")
+                        append(", ")
+
+                        append(plantPrediction?.data?.plant?.name ?: "Nama tanaman tidak tersedia")
+
+                        append(" biasanya ditanam dalam waktu ")
+
+                        // Menambahkan duration_plant dengan default value jika null
+                        append(plantPrediction?.data?.plant?.duration_plant?.toString() ?: "informasi waktu belum tersedia")
+                        append(" hari.")
+                    },
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+
+            }
         }
     }
+        }
 }
 
 @Preview
