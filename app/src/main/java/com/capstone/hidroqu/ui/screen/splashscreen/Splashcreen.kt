146977e7c441a366e90d59d09c2cb6ab895a999f
muammarRM <muammarramadhanimaulizidan@gmail.com>
2024-12-04 21:11:39 +0700

    fix: change splash screen image

--- app/src/main/java/com/capstone/hidroqu/ui/screen/splashscreen/Splashcreen.kt
+++ app/src/main/java/com/capstone/hidroqu/ui/screen/splashscreen/Splashcreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -12,6 +13,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -51,9 +53,11 @@ fun SplashScreen(
             .fillMaxSize(),
     ) {
         Image(
-            painter = painterResource(id = R.drawable.logo_circle),
+            painter = painterResource(id = R.drawable.ic_hidroqu),
             contentDescription = "App Logo",
-            modifier = Modifier.size(200.dp)
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
         )
     }
 }
